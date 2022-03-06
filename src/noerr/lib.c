65479897bef36b9bd12799c5f37468f1282a8c55
L-oris <loris4526@gmail.com>
2022-03-06 17:47:38 +0100

    add noerr_lib with tests

---
+++ src/noerr/lib.c
@@ -0,0 +1,23 @@
+#include "noerr.h"
+#include "str_safe.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static inline size_t strerrorlen(errno_t errnum) {
+    return strlen(strerror(errnum));
+}
+
+char* get_error(errno_t errnum) {
+    size_t size = strerrorlen(errnum) + 1;
+    char* msg = malloc(size);
+    if (msg == NULL) {
+        return NULL;
+    }
+
+    if (strerror_r(errnum, msg, size) != 0) {
+        strcpy_safe(msg, size, "unknown error");
+    }
+
+    return msg;
+}
