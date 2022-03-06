0a9b4d0de3d9fc46a895a406789535cfc0acc249
L-oris <loris4526@gmail.com>
2022-03-06 17:21:27 +0100

    compiler flags for different build modes; code snippets for sanitizers

---
+++ apps/data-race.c
@@ -0,0 +1,16 @@
+#include <pthread.h>
+
+int global;
+
+static void* thread1(void* x) {
+    global = 42;
+    return x;
+}
+
+int main() {
+    pthread_t t;
+    pthread_create(&t, NULL, thread1, NULL);
+    global = 43;
+    pthread_join(t, NULL);
+    return global;
+}
