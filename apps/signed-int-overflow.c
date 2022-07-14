8a7c648f94227dd6ba522fd1bcbdbcf907beea4c
dehre <loris4526@gmail.com>
2022-07-14 15:01:34 +0200

    remove dedicate cmakelists for apps directory

--- apps/signed-int-overflow.c
+++
@@ -1,7 +0,0 @@
-int main(int argc, char **argv)
-{
-    (void)argv;
-    int k = 0x7fffffff;
-    k += argc;
-    return 0;
-}
--- apps/use-after-free.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int main()
-{
-    char *x = malloc(10 * sizeof(char));
-    free(x);
-    return x[5];
-}
