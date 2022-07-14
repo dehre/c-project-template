9d142e6013a2dd1292f88ebf65b324f176eec9ff
dehre <loris4526@gmail.com>
2022-07-14 14:46:03 +0200

    remove old shell scripts; update tasks.json

--- docker-ubuntu-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run -ti -v "$PWD":/project ubuntu-cmake bash
-
-#
-# # to launch two or more sessions connected to the same container:
-#
-# docker exec -it <container-id> bash
-#
